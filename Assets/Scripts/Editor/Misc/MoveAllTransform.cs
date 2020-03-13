c3dae8b934801492929835dca39c5bf68c219484
TJ Helm <helmthomas2000@gmail.com>
2020-03-13 13:34:22 -0600

    Completed Organization

---
+++ Assets/Scripts/Editor/Misc/MoveAllTransform.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MoveAllTransform
+{
+    [MenuItem("Utils/Move All Transforms")]
+    public static void MoveAllTransforms()
+    {
+        foreach(Transform transform in Object.FindObjectsOfType<Transform>())
+        {
+            transform.position = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NW_Organization.Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        public float score;
+        public float maxPlayerHealth;
+
+        private float playerHealth;
+
+        public float PlayerHealth
+        {
+            get => playerHealth;
+            set
+            {
+                playerHealth = Mathf.Clamp(value, 0, maxPlayerHealth);
+            }
+        }
+
+        public enum State
+        {
+            moving,
+            still
+        }
+
+        public void KillPlayer()
+        {
+            //Destroy(player.gameObject);
+            PlayerHealth = 0;
+        }
+
+        //public void EatFsh(int amount)
+        //{
+        //    score += amount;
+        //}
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            
+        }
+    }
+}
